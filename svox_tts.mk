c03b380e9cb49267c9c15b8fb8011f89e6218b56
nift4 <nift4@protonmail.com>
2022-08-25 09:07:51 +0200

    fix privapp permissions
    Change-Id: I79cd2b7c7b827378ecf4a57b912988a2066e6869

--- svox_tts.mk
+++ svox_tts.mk
@@ -2,3 +2,6 @@ include external/svox/pico/lang/all_pico_languages.mk
 
 PRODUCT_PACKAGES += \
     PicoTts
+
+PRODUCT_COPY_FILES += \
+    external/svox/pico/privapp_whitelist_picotts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_picotts.xml
