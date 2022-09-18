f78da8190333cfc0987fbb41adc0a8030e282b74
Erfan Abdi <erfangplus@gmail.com>
2022-09-18 11:35:56 +0430

    pico: Set app sdk version back to 23
    * Also setup default permissions
    
    Change-Id: I4cc35ff9028fd01f224a8c3c8050b090a74599ca

--- svox_tts.mk
+++ svox_tts.mk
@@ -4,4 +4,5 @@ PRODUCT_PACKAGES += \
     PicoTts
 
 PRODUCT_COPY_FILES += \
-    external/svox/pico/privapp_whitelist_picotts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_picotts.xml
+    external/svox/pico/privapp_whitelist_picotts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_picotts.xml \
+    external/svox/pico/default_permissions_picotts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/default_permissions_picotts.xml
