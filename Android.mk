1ce78c136fa9a58d84ce9bcbc3ee866652cc12c1
althafvly <althafvly@gmail.com>
2025-09-25 13:45:03 +0530

    Convert build files to Android.bp

--- Android.mk
+++
@@ -1,6 +0,0 @@
-# Uncomment this to build single lang installer. Default is AIO
-#svox_lang_pack := PicoLangInstallerEngUsa
-
-LOCAL_PATH:= $(call my-dir)
-svox_lang_pack ?= picolanginstaller
-include $(LOCAL_PATH)/pico/Android.mk $(LOCAL_PATH)/$(svox_lang_pack)/Android.mk
